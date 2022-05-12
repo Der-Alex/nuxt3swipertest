eb98d7761d88cda48254800f4409d1d334e49a52
Der Alex <alexander.thimm@googlemail.com>
2022-05-12 12:23:10 +0200

    removed clientonly stuff

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,40 @@
 <template>
   <div>
     <h1>Home</h1>
-    <ClientOnly>
-      <div>
-        <swiper :slides-per-view="3" :auto-height="true" :centered-slides="true">
-          <swiper-slide>
-            <div class="blub">
-              <p>1</p>
-            </div>
-          </swiper-slide>
-          <swiper-slide>
-            <div class="blub">
-              <p>2</p>
-            </div>
-          </swiper-slide>
-          <swiper-slide>
-            <div class="blub">
-              <p>3</p>
-            </div>
-          </swiper-slide>
-          <swiper-slide>
-            <div class="blub">
-              <p>4</p>
-            </div>
-          </swiper-slide>
-          <swiper-slide>
-            <div class="blub">
-              <p>5</p>
-            </div>
-          </swiper-slide>
-          <swiper-slide>
-            <div class="blub">
-              <p>6</p>
-            </div>
-          </swiper-slide>
-        </swiper>
-      </div>
-    </ClientOnly>
+    <div>
+      <swiper :slides-per-view="3" :auto-height="true" :centered-slides="true">
+        <swiper-slide>
+          <div class="blub">
+            <p>1</p>
+          </div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="blub">
+            <p>2</p>
+          </div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="blub">
+            <p>3</p>
+          </div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="blub">
+            <p>4</p>
+          </div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="blub">
+            <p>5</p>
+          </div>
+        </swiper-slide>
+        <swiper-slide>
+          <div class="blub">
+            <p>6</p>
+          </div>
+        </swiper-slide>
+      </swiper>
+    </div>
   </div>
 </template>
 <script>
